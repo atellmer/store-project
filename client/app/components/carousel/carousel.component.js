2b9638d294e4965a2d8acbd45e342cdffccf0af6
Alex Plex <Alex Plex>
2016-06-06 18:39:14 +0500

    product-card added

---
+++ client/app/components/carousel/carousel.component.js
@@ -0,0 +1,13 @@
+;(function() {
+	'use strict';
+
+	$('.js-products__carousel').slick({
+		infinite: true,
+		speed: 1500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		dots: true
+	});
+})();
